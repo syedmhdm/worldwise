e59a3bb7fa6be9c384f7b8b7552252c985881228
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2023-10-23 23:49:26 +0530

    nested routes, index routes and Outlet Tag

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
         <Route path='login' element={<Login />} />
-        <Route path='app' element={<AppLayout />} />
+        <Route path='app' element={<AppLayout />}>
+          <Route index element={<p>Citiesssss</p>} />
+          <Route path='cities' element={<p>Citiesssss</p>} />
+          <Route path='countries' element={<p>countriesssss</p>} />
+          <Route path='form' element={<p>formzzz</p>} />
+        </Route>
         <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
