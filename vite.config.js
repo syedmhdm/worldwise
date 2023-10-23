9b7dba53869f1cd735cd6de0c1709e64886ea54a
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2023-10-24 01:21:11 +0530

    load data using json-server depdncy and set cities

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react()],
 });
