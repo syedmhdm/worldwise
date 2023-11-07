e88b6c5c6d3094c0e53e8e675685cc13db9e6843
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2023-11-07 23:57:17 +0530

    add new city through form and date picker

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -36,6 +36,25 @@ function CitiesProvider({ children }) {
       setIsLoading(false);
     }
   }
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: "POST",
+        body: JSON.stringify(newCity),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+
+      setCities((cities) => [...cities, data]);
+    } catch (error) {
+      alert("There was an error loading data...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   return (
     <CitiesContext.Provider
@@ -44,6 +63,7 @@ function CitiesProvider({ children }) {
         isLoading,
         currentCity,
         getCity,
+        createCity,
       }}
     >
       {children}
